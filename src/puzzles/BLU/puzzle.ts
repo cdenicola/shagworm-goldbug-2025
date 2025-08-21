5339970db528f346e657581bd0e6f423708d41cc
Cooper de Nicola <cdenicola@cs.stanford.edu>
2025-08-21 11:56:33 -0700

    puzzle markdown to html

--- src/puzzles/BLU/puzzle.ts
+++ src/puzzles/BLU/puzzle.ts
@@ -1,6 +1,4 @@
 import { TPuzzle } from "../../puzzle"
-import artifact1 from "./artifact1.png"
-import artifact2 from "./artifact2.png"
 
 const BLU: TPuzzle = {
   code: "BLU",
@@ -9,19 +7,6 @@ const BLU: TPuzzle = {
   anchor: "blu",
   difficulty: 2,
   markdownFile: "BLU.md",
-  artifacts: [
-    {
-      type: "image",
-      url: artifact1,
-      description: "The initial image provided in the puzzle.",
-    },
-    {
-      type: "image",
-      url: artifact2,
-      description:
-        "The scarabs with the correct color subtraction (ordered R, G, B from top to bottom).",
-    },
-  ],
 }
 
 export default BLU
